3d23be90b5b59c0a55f864cca410285adf32ebb4
minanader11 <nadermina30@gmail.com>
2024-02-20 11:49:03 +0200

    modal sheet

--- lib/core/show_modal_sheet.dart
+++ lib/core/show_modal_sheet.dart
@@ -1,9 +1,12 @@
+
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_app/features/modal_sheet.dart';
 
 void showModalSheet(BuildContext context) {
   showModalBottomSheet(
+    useRootNavigator: true,
     context: context,
     builder: (context) {
       return ModalSheet();
